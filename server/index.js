8c871a038595120bfb423072490a7595a2c387ff
karthik nadar <karthiknadar1204@gmail.com>
2026-01-03 16:29:25 +0530

    cors fix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import chatRoutes from './routes/chat.js'
 const app=express()
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type']
+}))
 app.use(express.json())
 app.use('/api',chatRoutes)
 
